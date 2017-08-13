f6cd81b906cd2da5ae04f7d9b3ec3fbb0313b0b0
Brayan Jesús Gutiérrez Esparza <bhn_rock@hotmail.com>
2017-08-12 22:53:39 -0600

    MVVM

--- ForeignExchangeXamarin/ForeignExchangeXamarin/ForeignExchangeXamarin/App.xaml.cs
+++ ForeignExchangeXamarin/ForeignExchangeXamarin/ForeignExchangeXamarin/App.xaml.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿
 
 namespace ForeignExchangeXamarin
 {
+    using Views;
+    using Xamarin.Forms;
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-            
-            MainPage = new ForeignExchangeXamarin.MainPage();
+
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
